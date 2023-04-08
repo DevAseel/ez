58f5fdc2426de3d0eab43d4dc62feeeadb40907c
DevAseel <99526005+DevAseel@users.noreply.github.com>
2023-04-08 05:50:29 +0400

    changed deployment region

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ const AuthShowcase: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
         onClick={
           sessionData
             ? () => void signOut()
