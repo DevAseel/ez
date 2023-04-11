215d0ffcfe465e29c07fd09b237da5c2c42ad9fd
DevAseel <99526005+DevAseel@users.noreply.github.com>
2023-04-12 02:43:20 +0400

    added logout

--- src/components/UserHero.tsx
+++ src/components/UserHero.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { Avatar, AvatarBadge, Stack, Text, Badge } from "@chakra-ui/react";
-import { Divider, Center, Button, Tooltip } from "@chakra-ui/react";
+import { Divider, Center, Button, Tooltip, Flex } from "@chakra-ui/react";
 import LocalFireDepartmentIcon from "@mui/icons-material/LocalFireDepartment";
 import WhatshotIcon from "@mui/icons-material/Whatshot";
+import Logout from "@mui/icons-material/Logout";
 import type { Points, Status } from "@prisma/client";
-import { useSession } from "next-auth/react";
-
+import { useSession, signOut } from "next-auth/react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from "@chakra-ui/react";
 type UserHeroProps = {
   pointsData: Points | null | undefined;
   statusData: Status | null | undefined;
@@ -14,6 +24,7 @@ type UserHeroProps = {
 
 const UserHero = ({ pointsData, statusData, haki }: UserHeroProps) => {
   const { data: sessionData } = useSession();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
       <Stack direction="row" spacing={4} justifyContent="center" paddingTop="8">
@@ -28,9 +39,14 @@ const UserHero = ({ pointsData, statusData, haki }: UserHeroProps) => {
           <AvatarBadge borderColor="#1D4044" boxSize="1.25em" bg="green.500" />
         </Avatar>
         <Stack direction="column" minW="8rem">
-          <Text fontSize="sm" fontWeight="bold">
-            {sessionData?.user.name}
-          </Text>
+          <Stack direction="row">
+            <Text fontSize="sm" fontWeight="bold">
+              {sessionData?.user.name}
+            </Text>
+            <Tooltip label="logout" placement="top">
+              <Logout onClick={onOpen} style={{ fontSize: "1rem" }} />
+            </Tooltip>
+          </Stack>
           <Badge
             bg="#1A202C"
             size="sm"
@@ -94,6 +110,41 @@ const UserHero = ({ pointsData, statusData, haki }: UserHeroProps) => {
           paddingY="2"
         />
       </Center>
+      <Modal
+        isCentered
+        onClose={onClose}
+        isOpen={isOpen}
+        motionPreset="slideInBottom"
+      >
+        <ModalOverlay />
+        <ModalContent bgColor="#171923">
+          <ModalHeader fontWeight="bold">Logout</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody fontSize="sm">
+            Leaving is never easy, but seeing you go feels like losing a slice
+            of our daily happiness pie. We will miss you and your unique flavor.
+            Safe travels, dear friend! 👋
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              colorScheme="teal"
+              mr={3}
+              size="sm"
+              onClick={() => void signOut()}
+            >
+              Leave
+            </Button>
+            <Button
+              colorScheme="teal"
+              variant="outline"
+              size="sm"
+              onClick={onClose}
+            >
+              Stay
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 };
