5bcd28410fb40305eddf77406393fb8016162e05
DevAseel <99526005+DevAseel@users.noreply.github.com>
2023-04-15 16:35:43 +0400

    added settings page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Flex, Text, Link } from "@chakra-ui/react";
+import { Flex, Text, Link as UILink } from "@chakra-ui/react";
+import Link from "next/link";
 
 const Header = () => {
   return (
@@ -10,24 +11,23 @@ const Header = () => {
       height="100%"
       px="12"
     >
-      <Text
-        fontSize="2xl"
-        fontWeight="bold"
-        textAlign="center"
-        px="2"
-        w={"12rem"}
-      >
-        LOGO
-      </Text>
-      <Flex
-        // width="15rem"
-        justifyContent="space-between"
-        alignItems="center"
-        fontSize="sm"
-      >
-        <Link pl="8">Profile</Link>
-        <Link pl="8">Status</Link>
-        <Link pl="8">Settings</Link>
+      <Link href="/">
+        <Text
+          fontSize="2xl"
+          fontWeight="bold"
+          textAlign="center"
+          px="2"
+          w={"12rem"}
+        >
+          LOGO
+        </Text>
+      </Link>
+      <Flex justifyContent="space-between" alignItems="center" fontSize="sm">
+        <UILink pl="8">Profile</UILink>
+        <UILink pl="8">Status</UILink>
+        <Link href="/settings">
+          <UILink pl="8">Settings</UILink>
+        </Link>
       </Flex>
     </Flex>
   );
---
+++ src/pages/settings/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Main from "~/layouts/Main";
+const Settings = () => {
+  return <Main>settings page</Main>;
+};
+
+export default Settings;
