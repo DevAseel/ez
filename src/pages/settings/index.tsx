b6c62eb097a8cd54d385a5817b9390c698cedcc1
DevAseel <99526005+DevAseel@users.noreply.github.com>
2023-04-21 23:57:36 +0400

    added checks for user input

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -10,11 +10,9 @@ import {
   InputGroup,
   InputLeftAddon,
   Input,
-  Flex,
-  Select,
-  Textarea,
-  Button,
 } from "@chakra-ui/react";
+import { useToast, Flex, Select, Textarea, Button } from "@chakra-ui/react";
+
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import { timezones, type Timezone } from "~/utils/timezones";
 import { api } from "~/utils/api";
@@ -28,10 +26,12 @@ type userInput = {
   location: string;
   githubAccount: string;
 };
+
 const Settings = () => {
   const tz: Timezone[] = timezones;
   const utils = api.useContext();
   const { data: sessionData } = useSession();
+  const toast = useToast();
 
   const { data: userSettings } = api.settings.userSettings.useQuery(undefined, {
     enabled: sessionData?.user !== undefined,
@@ -52,6 +52,7 @@ const Settings = () => {
     event.preventDefault();
     const formData = new FormData(event.target as HTMLFormElement);
     const data = Object.fromEntries(formData.entries()) as userInput;
+    // check if this's the first time user updates the settings
     if (!userSettings) {
       createUserSettings.mutate({
         bio: data.bio,
@@ -60,14 +61,43 @@ const Settings = () => {
         location: data.location,
         githubAccount: data.githubAccount,
       });
+      toast({
+        title: "Profile updated!",
+        description: "Congratulations on your first profile update!",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+      });
     }
-    updateUserSettings.mutate({
-      bio: data.bio,
-      workingHours: data.workingHours,
-      timeZone: data.timeZone,
-      location: data.location,
-      githubAccount: data.githubAccount,
-    });
+    if (userSettings) {
+      const { userId, ...rest } = userSettings;
+      const userSettingsData = rest;
+
+      if (JSON.stringify(userSettingsData) !== JSON.stringify(data)) {
+        updateUserSettings.mutate({
+          bio: data.bio,
+          workingHours: data.workingHours,
+          timeZone: data.timeZone,
+          location: data.location,
+          githubAccount: data.githubAccount,
+        });
+        toast({
+          title: "Profile updated!",
+          description: "Awesome, your profile is now updated!",
+          status: "success",
+          duration: 9000,
+          isClosable: true,
+        });
+      } else {
+        toast({
+          title: "Oops!",
+          description: "It looks like your profile is up to date!",
+          status: "warning",
+          duration: 9000,
+          isClosable: true,
+        });
+      }
+    }
   };
   return (
     <Main>
@@ -110,31 +140,44 @@ const Settings = () => {
             Bio
           </Text>
           <Textarea
-            placeholder="tell us about yourself"
+            placeholder={
+              userSettings?.bio ? userSettings.bio : "tell us about yourself"
+            }
             borderColor="#2D3748"
             fontWeight="light"
             name="bio"
+            required
           />
 
           <Text fontSize="sm" pb="2" fontWeight="bold" paddingTop="4">
             Working Hours
           </Text>
           <Input
-            placeholder="What's your working hours?"
+            placeholder={
+              userSettings?.workingHours
+                ? userSettings.workingHours
+                : "What's your working hours?"
+            }
             fontWeight="light"
             borderColor="#2D3748"
             width="max-width"
             name="workingHours"
+            required
           />
           <Text fontSize="sm" pb="2" fontWeight="bold" paddingTop="4">
             Timezone
           </Text>
           <Select
-            placeholder="select your timezone"
+            placeholder={
+              userSettings?.timeZone
+                ? userSettings.timeZone
+                : "select your timezone"
+            }
             borderColor="#2D3748"
             fontWeight="light"
             color="#718096"
             name="timeZone"
+            required
           >
             {tz.map((zone, index) => {
               return (
@@ -148,11 +191,16 @@ const Settings = () => {
             Location
           </Text>
           <Input
-            placeholder="What's your working hours?"
+            placeholder={
+              userSettings?.workingHours
+                ? userSettings.workingHours
+                : "What's your working hours?"
+            }
             fontWeight="light"
             borderColor="#2D3748"
             width="max-width"
             name="location"
+            required
           />
 
           <Text fontSize="sm" pb="2" fontWeight="bold" paddingTop="4">
@@ -161,7 +209,6 @@ const Settings = () => {
           <InputGroup size="sm">
             <InputLeftAddon
               bgColor="#2D3748"
-              placeholder="What's your status?"
               borderColor="#2D3748"
               height="auto"
               fontWeight="light"
@@ -169,10 +216,15 @@ const Settings = () => {
               github.com/
             </InputLeftAddon>
             <Input
-              placeholder="your github username"
+              placeholder={
+                userSettings?.githubAccount
+                  ? userSettings.githubAccount
+                  : "What's your github username?"
+              }
               fontWeight="light"
               borderColor="#2D3748"
               name="githubAccount"
+              required
             />
           </InputGroup>
           <Button
